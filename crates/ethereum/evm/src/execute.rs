8ffbc3777294e3bbdf3f4cea65393f8301881dff
hermitsgame <g.wang.x@gmail.com>
2025-12-06 15:19:58 +0000

    Fix EthExecuteOutput pattern matching in EthBatchExecutor

--- crates/ethereum/evm/src/execute.rs
+++ crates/ethereum/evm/src/execute.rs
@@ -437,7 +437,7 @@ where
 
     fn execute_and_verify_one(&mut self, input: Self::Input<'_>) -> Result<(), Self::Error> {
         let BlockExecutionInput { block, total_difficulty } = input;
-        let EthExecuteOutput { receipts, requests, gas_used: _ } =
+        let EthExecuteOutput { receipts, requests, gas_used: _, valid_transaction_indices: _ } =
             self.executor.execute_without_verification(block, total_difficulty)?;
 
         validate_block_post_execution(block, self.executor.chain_spec(), &receipts, &requests)?;
